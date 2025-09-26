a63f36e156836d1c7290b4f94e9005740c6b512f
Joan Mateu <joan.mateu.jordi@gmail.com>
2025-09-26 16:57:40 +0200

    fix error react Component holdings

--- frontend/src/components/HoldingsTable.tsx
+++ frontend/src/components/HoldingsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import type { Holding, HoldingsResponse } from "../types";
 import RefreshButton from "./RefreshButton";
 import { getHoldings } from "../api";
